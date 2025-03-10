e71ff83d8d0e348b192463b9545c08d0d69335a9
rexchoppers <connorbrady@codox.uk>
2025-03-10 13:49:04 +0000

    UPDATED Access to take into account loading of relations or not

--- src/types/Access.ts
+++ src/types/Access.ts
@@ -1,9 +1,10 @@
 import {Company} from "./Company.ts";
+import {User} from "./User.ts";
 
 export interface Access {
     id: string;
-    company: Company;
-    user: string;
+    company?: Company | string;
+    user?: User | string;
     level: string;
     createdAt: string;
     updatedAt: string;
